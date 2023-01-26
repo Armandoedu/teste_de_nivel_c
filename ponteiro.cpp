63c9044b86084c29ec37c60b207c8129f36d4106
Armandoedu <113314643+Armandoedu@users.noreply.github.com>
2023-01-26 15:22:43 -0300

    Add files via upload

---
+++ ponteiro.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int mult(int x, int y){
+	return x * y;
+}
+
+
+int soma(int a, int b){
+	return a + b;
+}
+
+
+void calculo(int x, int y,int(*ponteiro)(int, int)){
+	
+	
+	(*ponteiro)(x, y);
+	
+}
+
+
+int main(){
+	int result = calculo(5, 5, &mult);
+	int resultado = calculo(5, 3, soma);
+	
+	printf("Resultado: %d\n", resultado);
+	printf("Resultado: %d", result);
+}
