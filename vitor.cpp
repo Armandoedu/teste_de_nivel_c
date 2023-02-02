6386983e5dcefa2f1c77403fd75bd666b7de46b5
Armandoedu <113314643+Armandoedu@users.noreply.github.com>
2023-02-02 17:22:01 +0000

    Add files via upload

---
+++ vitor.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	int i;
+	int *vetor = (int*) calloc(10, sizeof(int));
+	
+	if(vetor == NULL){
+		printf("ERROR\n");
+		exit(1);
+	}
+	
+	for(i=0; i<10; i++){
+		vetor[i]=i;
+	}
+	for(i=0; i < 10; i++){
+		printf("%d ", vetor[i]);
+	}
+	free(vetor);
+}
